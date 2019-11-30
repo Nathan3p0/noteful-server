f88277fc2894336117a071c8071fe73a76434875
Nathan Szelag <nszelag@live.com>
2019-11-30 12:57:28 -0500

    updated database url

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const knex = require('knex')
 const app = require('./app')
-const { PORT, NODE_ENV } = require('./config')
-const knexConfig = require('../knexfile')
+const { PORT, NODE_ENV, DATABASE_URL } = require('./config')
 
-const db = knex(knexConfig[NODE_ENV])
+const db = knex({
+    client: 'pg',
+    connection: DATABASE_URL
+})
 
 app.set('db', db)
 
